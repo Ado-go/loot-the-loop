a5948da4514dca12b3b712cbd9a86f87b91f262d
Adogo <gonda.ado@gmail.com>
2026-01-15 00:05:29 +0100

    feat: tailwind + basic game layout + explore logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   const [deck, setDeck] = useState<
@@ -44,6 +44,24 @@ function App() {
     setDeck(cards);
   }
 
+  function explore() {
+    if (!deck[0].revealed) {
+      if (!deck[1].revealed) {
+        setDeck(
+          deck.map((card, index) =>
+            index < 2 ? { ...card, revealed: true } : card
+          )
+        );
+      } else {
+        setDeck(
+          deck.map((card, index) =>
+            index < 1 ? { ...card, revealed: true } : card
+          )
+        );
+      }
+    }
+  }
+
   function startGame() {
     setGameEnd(false);
   }
@@ -51,22 +69,53 @@ function App() {
   console.log(deck);
   return (
     <div>
-      <h1>Loot the loop card game</h1>
       {gameEnd && (
-        <button
-          onClick={() => {
-            shuffleCards();
-            startGame();
-          }}
-        >
-          Start the game
-        </button>
+        <div className="min-h-screen flex flex-col gap-5 items-center justify-center">
+          <h1>Loot the loop card game</h1>
+          <button
+            onClick={() => {
+              shuffleCards();
+              startGame();
+            }}
+          >
+            Start the game
+          </button>
+        </div>
       )}
       {!gameEnd && (
-        <div>
-          <div>Temple:</div>
-          <div>Notes:</div>
-          <div>Score Pile:</div>
+        <div className="flex flex-col gap-20">
+          <div>
+            <h2>Temple:</h2>
+            <div className="flex p-5 w-86 overflow-hidden overflow-x-scroll border">
+              {deck.map((card, index) => (
+                <Card
+                  key={card.suit + card.value}
+                  suit={card.suit}
+                  value={card.value}
+                  revealed={card.revealed}
+                  position={index + 1}
+                />
+              ))}
+            </div>
+            <button onClick={() => explore()}>
+              Look around(reveal top card/s)
+            </button>
+            <button>Explore(move x-amount forward in deck)</button>
+            <button>Mark path(move top card to notes)</button>
+            <button>
+              Return to marked path(return card from notes to top)
+            </button>
+          </div>
+          <div className="flex justify-around">
+            <div>
+              <h2>Notes (3 spaces):</h2>
+            </div>
+            <div>
+              <h2>Score Pile:</h2>
+              <div>Trinkets:</div>
+              <div>Jewels:</div>
+            </div>
+          </div>
         </div>
       )}
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,8 +2,16 @@ type CardProps = {
   suit: string;
   value: string;
   revealed: boolean;
+  position: number;
 };
 
-export default function Card({ suit, value, revealed }: CardProps) {
-  <div>{revealed ? suit + value : "??"}</div>;
+export default function Card({ suit, value, revealed, position }: CardProps) {
+  return (
+    <>
+      <div className="w-10 h-32 bg-gray-200 border">
+        <span className="relative bottom-5">{position}</span>
+        {revealed ? suit + value : "??"}
+      </div>
+    </>
+  );
 }
