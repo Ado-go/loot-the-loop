d7b89c04424553f85de2ecd8653443e200e7c17a
Adogo <gonda.ado@gmail.com>
2026-01-16 16:59:16 +0100

    feat: option menu dispaly change

--- src/App.tsx
+++ src/App.tsx
@@ -154,14 +154,8 @@ function App() {
         </div>
       )}
       {!gameEnd && (
-        <div className="min-h-screen flex flex-col pt-5 items-center gap-10">
-          <button
-            onClick={() => setOpenOptions(true)}
-            className="absolute flex justify-center items-center w-10 h-10 z-5 top-5 right-5"
-          >
-            <p>⚙</p>
-          </button>
-          {openOptions && (
+        <div className="min-h-screen flex flex-col items-center gap-10">
+          {openOptions ? (
             <div className="w-full h-full flex flex-col items-center justify-center gap-2 bg-gray-800 absolute z-10">
               <button
                 onClick={() => {
@@ -173,145 +167,158 @@ function App() {
               </button>
               <button onClick={() => setOpenOptions(false)}>Close</button>
             </div>
+          ) : (
+            <>
+              <button
+                onClick={() => setOpenOptions(true)}
+                className="absolute flex justify-center items-center w-10 h-10 z-5 top-5 right-5"
+              >
+                <p>⚙</p>
+              </button>
+              <div className="flex flex-col">
+                <h2 className="text-center">Temple</h2>
+                <div className="flex p-5 max-w-86 overflow-hidden overflow-x-scroll border">
+                  {deck.map((card, index) => (
+                    <Card
+                      key={card.suit + card.value}
+                      suit={card.suit}
+                      value={card.value}
+                      revealed={card.revealed}
+                      position={index + 1}
+                    />
+                  ))}
+                </div>
+                <div className="max-w-86 flex flex-col items-center">
+                  <div>
+                    <button
+                      className=" disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={isEnd || deck[0].revealed}
+                      onClick={() => lookAround()}
+                    >
+                      Look around(reveal top card/s)
+                    </button>
+                  </div>
+                  <div className="flex">
+                    <button
+                      disabled={
+                        isEnd ||
+                        !deck[0].revealed ||
+                        isNaN(parseInt(deck[0].value))
+                      }
+                      className="disabled:opacity-50 disabled:cursor-not-allowed"
+                      onClick={() => explore(parseInt(deck[0].value))}
+                    >
+                      explore with first card
+                      {deck[0].revealed &&
+                        !isNaN(parseInt(deck[0].value)) &&
+                        "(" + deck[0].value + ")"}
+                    </button>
+                    <button
+                      disabled={
+                        isEnd ||
+                        !deck[1].revealed ||
+                        isNaN(parseInt(deck[1].value))
+                      }
+                      className="disabled:opacity-50 disabled:cursor-not-allowed"
+                      onClick={() => explore(parseInt(deck[1].value))}
+                    >
+                      explore with second card
+                      {deck[1].revealed &&
+                        !isNaN(parseInt(deck[1].value)) &&
+                        "(" + deck[1].value + ")"}
+                    </button>
+                  </div>
+                  <div>
+                    <button
+                      className="disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={
+                        isEnd ||
+                        markedPaths.length === 3 ||
+                        !deck[0].revealed ||
+                        isNaN(parseInt(deck[0].value))
+                      }
+                      onClick={() => markPath()}
+                    >
+                      Mark path(move top card to notes)
+                    </button>
+                  </div>
+                  <div className="flex">
+                    <button
+                      className="disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={isEnd || markedPaths[0]?.value === undefined}
+                      onClick={() => returnToMarkedPath(0)}
+                    >
+                      Return to #1 path({markedPaths[0]?.value})
+                    </button>
+                    <button
+                      className="disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={isEnd || markedPaths[1]?.value === undefined}
+                      onClick={() => returnToMarkedPath(1)}
+                    >
+                      Return to #2 path({markedPaths[1]?.value})
+                    </button>
+                    <button
+                      className="disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled={isEnd || markedPaths[2]?.value === undefined}
+                      onClick={() => returnToMarkedPath(2)}
+                    >
+                      Return to #3 path({markedPaths[2]?.value})
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-5 justify-around">
+                <div>
+                  <h2>Notes ({3 - markedPaths.length} free spaces):</h2>
+                  <div>
+                    <h3>Paths:</h3>
+                    <div className="flex p-5 w-40 overflow-hidden overflow-x-auto border">
+                      {markedPaths.map((path, index) => (
+                        <Card
+                          key={path.suit + path.value}
+                          suit={path.suit}
+                          value={path.value}
+                          revealed={path.revealed}
+                          position={index + 1}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h2>Score Pile:</h2>
+                  <div>
+                    <h3>Trinkets:</h3>
+                    <div className="flex p-5 w-40 overflow-hidden overflow-x-auto border">
+                      {trinkets.map((trinket, index) => (
+                        <Card
+                          key={trinket.suit + trinket.value}
+                          suit={trinket.suit}
+                          value={trinket.value}
+                          revealed={trinket.revealed}
+                          position={index + 1}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="text-blue-400">Jewels:</h3>
+                    <div className="flex p-5 w-40 overflow-hidden overflow-x-auto border">
+                      {jewels.map((jewel, index) => (
+                        <Card
+                          key={jewel.suit + jewel.value}
+                          suit={jewel.suit}
+                          value={jewel.value}
+                          revealed={jewel.revealed}
+                          position={index + 1}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
           )}
-          <div className="flex flex-col">
-            <h2 className="text-center">Temple</h2>
-            <div className="flex p-5 max-w-90 overflow-hidden overflow-x-scroll border">
-              {deck.map((card, index) => (
-                <Card
-                  key={card.suit + card.value}
-                  suit={card.suit}
-                  value={card.value}
-                  revealed={card.revealed}
-                  position={index + 1}
-                />
-              ))}
-            </div>
-            <div className="max-w-90 flex flex-col items-center">
-              <div>
-                <button
-                  className=" disabled:opacity-50 disabled:cursor-not-allowed"
-                  disabled={isEnd || deck[0].revealed}
-                  onClick={() => lookAround()}
-                >
-                  Look around(reveal top card/s)
-                </button>
-              </div>
-              <div className="flex">
-                <button
-                  disabled={
-                    isEnd || !deck[0].revealed || isNaN(parseInt(deck[0].value))
-                  }
-                  className="disabled:opacity-50 disabled:cursor-not-allowed"
-                  onClick={() => explore(parseInt(deck[0].value))}
-                >
-                  explore with first card
-                  {deck[0].revealed &&
-                    !isNaN(parseInt(deck[0].value)) &&
-                    "(" + deck[0].value + ")"}
-                </button>
-                <button
-                  disabled={
-                    isEnd || !deck[1].revealed || isNaN(parseInt(deck[1].value))
-                  }
-                  className="disabled:opacity-50 disabled:cursor-not-allowed"
-                  onClick={() => explore(parseInt(deck[1].value))}
-                >
-                  explore with second card
-                  {deck[1].revealed &&
-                    !isNaN(parseInt(deck[1].value)) &&
-                    "(" + deck[1].value + ")"}
-                </button>
-              </div>
-              <div>
-                <button
-                  className="disabled:opacity-50 disabled:cursor-not-allowed"
-                  disabled={
-                    isEnd ||
-                    markedPaths.length === 3 ||
-                    !deck[0].revealed ||
-                    isNaN(parseInt(deck[0].value))
-                  }
-                  onClick={() => markPath()}
-                >
-                  Mark path(move top card to notes)
-                </button>
-              </div>
-              <div className="flex">
-                <button
-                  className="disabled:opacity-50 disabled:cursor-not-allowed"
-                  disabled={isEnd || markedPaths[0]?.value === undefined}
-                  onClick={() => returnToMarkedPath(0)}
-                >
-                  Return to #1 path({markedPaths[0]?.value})
-                </button>
-                <button
-                  className="disabled:opacity-50 disabled:cursor-not-allowed"
-                  disabled={isEnd || markedPaths[1]?.value === undefined}
-                  onClick={() => returnToMarkedPath(1)}
-                >
-                  Return to #2 path({markedPaths[1]?.value})
-                </button>
-                <button
-                  className="disabled:opacity-50 disabled:cursor-not-allowed"
-                  disabled={isEnd || markedPaths[2]?.value === undefined}
-                  onClick={() => returnToMarkedPath(2)}
-                >
-                  Return to #3 path({markedPaths[2]?.value})
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="flex gap-5 justify-around">
-            <div>
-              <h2>Notes ({3 - markedPaths.length} free spaces):</h2>
-              <div>
-                <h3>Paths:</h3>
-                <div className="flex p-5 w-40 overflow-hidden overflow-x-auto border">
-                  {markedPaths.map((path, index) => (
-                    <Card
-                      key={path.suit + path.value}
-                      suit={path.suit}
-                      value={path.value}
-                      revealed={path.revealed}
-                      position={index + 1}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div>
-              <h2>Score Pile:</h2>
-              <div>
-                <h3>Trinkets:</h3>
-                <div className="flex p-5 w-40 overflow-hidden overflow-x-auto border">
-                  {trinkets.map((trinket, index) => (
-                    <Card
-                      key={trinket.suit + trinket.value}
-                      suit={trinket.suit}
-                      value={trinket.value}
-                      revealed={trinket.revealed}
-                      position={index + 1}
-                    />
-                  ))}
-                </div>
-              </div>
-              <div>
-                <h3 className="text-blue-400">Jewels:</h3>
-                <div className="flex p-5 w-40 overflow-hidden overflow-x-auto border">
-                  {jewels.map((jewel, index) => (
-                    <Card
-                      key={jewel.suit + jewel.value}
-                      suit={jewel.suit}
-                      value={jewel.value}
-                      revealed={jewel.revealed}
-                      position={index + 1}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       )}
     </>
